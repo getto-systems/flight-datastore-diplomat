dd1391a247ef51152cab8bc9c05fa94d4b36ab3e
shun <shun@getto.systems>
2017-08-09 14:38:26 +0900

    add test

--- lib/flight_datastore/find.ex
+++ lib/flight_datastore/find.ex
@@ -1,4 +1,12 @@
 defmodule FlightDatastore.Find do
+  @moduledoc """
+  Find entity utils
+  """
+
+  @doc """
+  Find by Diplomat
+  return nil if error or not exists
+  """
   def find_entity(kind,key) do
     Diplomat.Key.new(kind,key)
     |> Diplomat.Key.get
@@ -9,6 +17,29 @@ defmodule FlightDatastore.Find do
     |> Enum.at(0)
   end
 
+  @doc """
+  Check for entity satisfy conditions
+
+  ## Examples
+
+      iex> FlightDatastore.Find.check(%{properties: %{"col" => %{value: "val"}}}, nil)
+      %{properties: %{"col" => %{value: "val"}}}
+
+      iex> FlightDatastore.Find.check(%{properties: %{"col" => %{value: "val"}}}, %{})
+      %{properties: %{"col" => %{value: "val"}}}
+
+      iex> FlightDatastore.Find.check(%{properties: %{"col" => %{value: "val"}}}, %{"col" => "val"})
+      %{properties: %{"col" => %{value: "val"}}}
+
+      iex> FlightDatastore.Find.check(%{properties: %{"col" => %{value: "val"}}}, %{"col" => "not matches"})
+      nil
+
+      iex> FlightDatastore.Find.check(%{properties: %{"col" => %{value: "val"}}}, %{"unknown col" => "not matches"})
+      nil
+
+      iex> FlightDatastore.Find.check(nil, %{"unknown col" => "not matches"})
+      nil
+  """
   def check(entity, conditions) do
     if entity do
       case conditions do
@@ -33,6 +64,29 @@ defmodule FlightDatastore.Find do
     end)
   end
 
+  @doc """
+  Convert entity to map
+
+  ## Examples
+
+      iex> FlightDatastore.Find.to_map(%{properties: %{"col" => %{value: "val"}}}, ["col"])
+      %{"col" => "val"}
+
+      iex> FlightDatastore.Find.to_map(%{properties: %{"col" => %{value: "val"}}}, ["col","unknown"])
+      %{"col" => "val"}
+
+      iex> FlightDatastore.Find.to_map(%{properties: %{"col" => %{value: "val"}}}, ["unknown"])
+      %{}
+
+      iex> FlightDatastore.Find.to_map(%{properties: %{"col" => %{value: "val"}}}, [])
+      %{}
+
+      iex> FlightDatastore.Find.to_map(%{properties: %{"col" => %{value: "val"}}}, nil)
+      %{}
+
+      iex> FlightDatastore.Find.to_map(nil, nil)
+      nil
+  """
   def to_map(entity, columns) do
     if entity do
       case columns do
---
+++ test/flight_datastore_find_test.exs
@@ -0,0 +1,4 @@
+defmodule FlightDatastore.FindTest do
+  use ExUnit.Case
+  doctest FlightDatastore.Find
+end
